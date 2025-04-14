fdc9b552b02a0f0d4824afb30d3aad0f33ea33e5
zhengxin-plog <72852489+zhengxin-plog@users.noreply.github.com>
2025-04-14 17:13:30 +0800

    Add files via upload

---
+++ MDCT-DTA-main/MDCT-DTA/method/model/test.py
@@ -0,0 +1,84 @@
+# %%
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = "0,1,2,3"
+import numpy as np
+import torch
+import torch.nn as nn
+from torch_geometric.data import DataLoader
+import argparse
+import matplotlib.pyplot as plt
+import PySide2
+import seaborn as sns
+
+from metrics import get_cindex, get_rm2
+from dataset import *
+
+from ML_DTI import MGraphDTA
+from utils import *
+
+dirname = os.path.dirname(PySide2.__file__)
+plugin_path = os.path.join(dirname, 'plugins', 'platforms')
+os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
+
+def val(model, criterion, dataloader, device):
+    model.eval()
+    running_loss = AverageMeter()
+
+    pred_list = []
+    label_list = []
+
+    for data in dataloader:
+        data = data.to(device)
+
+        with torch.no_grad():
+            pred = model(data)
+            loss = criterion(pred.view(-1), data.y.view(-1))
+            label = data.y
+            pred_list.append(pred.view(-1).detach().cpu().numpy())
+            label_list.append(label.detach().cpu().numpy())
+            running_loss.update(loss.item(), label.size(0))
+
+    pred = np.concatenate(pred_list, axis=0)
+    label = np.concatenate(label_list, axis=0)
+    plt.figure(figsize=(15, 12), dpi=80)
+    sns.jointplot(x=pred, y=label, kind="scatter")
+    plt.xlabel("X", size=16)
+    plt.ylabel("y", size=16)
+    plt.savefig("/21TB/zx/code/DMDTA/DMDTA/regression/joint_plot.png")
+    epoch_cindex = get_cindex(label, pred)
+    epoch_r2 = get_rm2(label, pred)
+    epoch_loss = running_loss.get_average()
+    running_loss.reset()
+
+    return epoch_loss, epoch_cindex, epoch_r2
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    # Add argument
+    parser.add_argument('--dataset', required=True, help='davis or kiba')
+    parser.add_argument('--model_path', required=True, type=str, help='model path ready to load')
+    args = parser.parse_args()
+
+    data_root = "data"
+    DATASET = args.dataset
+    model_path = args.model_path
+
+    fpath = os.path.join(data_root, DATASET)
+
+    test_set = GNNDataset(fpath, train=False)
+    print("Number of test: ", len(test_set))
+    test_loader = DataLoader(test_set, batch_size=256, shuffle=False, num_workers=8)
+
+    device = torch.device('cuda:2')
+    model = MGraphDTA(3, 25 + 1, embedding_size=128, filter_num=32, out_dim=1).to(device)
+
+    criterion = nn.MSELoss()
+    load_model_dict(model, model_path)
+    test_loss, test_cindex, test_r2 = val(model, criterion, test_loader, device)
+    msg = "test_loss:%.4f, test_cindex:%.4f, test_r2:%.4f" % (test_loss, test_cindex, test_r2)
+    print(msg)
+
+
+if __name__ == "__main__":
+    main()
